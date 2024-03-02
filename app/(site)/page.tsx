dcc617be8474264de2f69314ef7266a92c6ccfd1
Daniel-Zelalem <106803484+Daniel-Zelalem@users.noreply.github.com>
2024-03-02 11:23:05 +0300

    Auth pageUpdate

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     >
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <FaJediOrder size={100} className="mx-auto w-auto" />
-        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-400">
           Authentication
         </h2>
       </div>
