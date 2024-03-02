e48c6c16d2e40beaa02627bd8611cf56d8d2461f
Daniel-Zelalem <106803484+Daniel-Zelalem@users.noreply.github.com>
2024-03-02 11:19:00 +0300

    Update authentication page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <FaJediOrder size={100} className="mx-auto w-auto" />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-          Your's Authentication
+          Authentication
         </h2>
       </div>
       <AuthForm />
